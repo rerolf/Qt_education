b1d52855e74c70640fd7056c84903c8c4c5946ef
rerolf <bingodingo365@gmail.com>
2020-05-05 20:07:07 -0400

    Created a button app that increments the value when u click it

---
+++ Button-app/Button-app/main.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include "Counter.h"
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QLabel lbl("0");
+    QPushButton cmd("ADD");
+    Counter counter;
+    lbl.show();
+    cmd.show();
+    QObject::connect(&cmd,SIGNAL(clicked())
+                     ,&counter, SLOT(slotInc())
+                     );
+    QObject::connect(&counter,SIGNAL(counterChanged(int))
+                     , &lbl, SLOT(setNum(int))
+                     );
+    QObject::connect(&counter,SIGNAL(goodbye())
+                     , &app, SLOT(quit())
+                     );
+    return app.exec();
+}
